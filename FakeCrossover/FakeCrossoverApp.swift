cb5215172c13f6f9e09393e4157e22d569847bec
GUNNA-at <a.kolodii@icloud.com>
2025-12-23 15:39:31 +0100

    Initial Commit

---
+++ FakeCrossover/FakeCrossoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeCrossoverApp.swift
+//  FakeCrossover
+//
+//  Created by Gunna on 23.12.25.
+//
+
+import SwiftUI
+
+@main
+struct FakeCrossoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
